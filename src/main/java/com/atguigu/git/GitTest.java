14e058bc2248fc8905d32e158b143636bc45c605
leoqin <1730590229@qq.com>
2022-06-02 11:15:27 +0800

    推送远程库

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("第二次");
         System.out.println("第三次");
         System.out.println("master添加");
+        System.out.println("测试本地库推送远程库");
     }
 }
