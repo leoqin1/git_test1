997d7923f971fccb96afa4fc326b4f253a04a673
leoqin <1730590229@qq.com>
2022-06-02 10:22:45 +0800

    master添加

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,8 +3,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("新建文件");
-        System.out.println("第一次");
         System.out.println("第二次");
-        System.out.println("dev添加");
+        System.out.println("第三次");
+        System.out.println("master添加");
     }
 }
